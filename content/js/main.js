ad51fd4876b26ffaef68bbf2e23f99de8bfc4d0d
MilesWilford <miles.wilford@gmail.com>
2013-05-19 13:17:27 -0400

    Started movie page template

--- content/js/main.js
+++ content/js/main.js
@@ -1,1 +1,23 @@
-
+$(document).ready(function() {
+    (function() {
+        // Fill out our ratings
+        $('.rating').each(function() {
+            var $span = $(this).find('span');
+            var rating = parseFloat($span.text());
+            $span.remove();
+            var starCount = '<span class="stars-earned">';
+            for (var i = 1; i <= rating; i++) {
+                starCount += "*";
+            }
+            starCount += '</span><span class="stars-unearned">';
+            for (var i = rating; i <= 10; i++) {
+                starCount += "*";
+            }
+            starCount += '</span>'
+                + '<span class="stars-numerical">' + rating
+                + '<span class="total">/10</span></span>'
+                + '<a href="#">Rating Info</a>';
+            $(this).html(starCount);
+        });
+    })();
+});
